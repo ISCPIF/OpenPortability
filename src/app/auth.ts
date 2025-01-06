226f46f48b28329d055730b21bc3870047da4d90
Cedric Rossi <cedric@i9n.fr>
2025-01-06 18:04:26 +0100

    WIP: serveur mastodon dynamique

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -2,12 +2,73 @@ import NextAuth from "next-auth"
 import type { Profile } from "next-auth"
 import { authConfig } from "./auth.config"
 import type { TwitterData, MastodonProfile, BlueskyProfile } from "@/lib/supabase-adapter"
+import { supabase } from "@/lib/supabase"
 
 export type { TwitterData as TwitterProfile } from "@/lib/supabase-adapter"
 export type { MastodonProfile }
 export type { BlueskyProfile }
 
-export const { auth, signIn, signOut, handlers } = NextAuth(authConfig)
+
+async function createMastodonApp(instance: string){
+const { data: instances } = await supabase.from("mastodon_instances").select();
+let cachedValue = instances.find(r => r.instance == instance)
+  if (!cachedValue) {
+    console.log("no cached value")
+    const url = `https://${instance}/api/v1/apps`
+    const formData = {
+        "client_name": "HelloQuitX",
+        "redirect_uris": [`${process.env.NEXTAUTH_URL}/api/auth/callback/mastodon`],
+        // TODO: limiter ?
+        "scopes": "read",
+        "website": "https://app.helloquitx.com"
+    }
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        console.log(json)
+        cachedValue = {
+            instance,
+            client_id: json.client_id,
+            client_secret: json.client_secret
+        }
+
+        const insertResult = await supabase.from("mastodon_instances").insert(cachedValue);
+        console.log("inserted", insertResult)
+        } catch (error) {
+        console.error(error.message);
+        }
+  }
+  return cachedValue
+}
+
+// https://authjs.dev/reference/nextjs#lazy-initialization
+export const { auth, signIn, signOut, handlers } = NextAuth(async req => {
+    if (req?.url.endsWith("api/auth/signin/mastodon")) {
+        console.log(req) // do something with the request
+        const instance = "mamot.fr"
+        const res = await createMastodonApp(instance)
+        console.log(res)
+        const mastodonProvider = authConfig.providers.find(prov => prov.id === "mastodon")
+        mastodonProvider.issuer = `https://${instance}`;
+        mastodonProvider.clientId = res.client_id;
+        mastodonProvider.clientSecret = res.client_secret;
+        mastodonProvider.authorization = `${mastodonProvider.issuer}/oauth/authorize?scope=read`;
+        mastodonProvider.token = `${mastodonProvider.issuer}/oauth/token`;
+        mastodonProvider.userinfo = `${mastodonProvider.issuer}/api/v1/accounts/verify_credentials`;
+        console.log(authConfig)
+
+    }
+    return authConfig;
+})
 
 // Type guards
 export function isTwitterProfile(profile: any): profile is TwitterData {
