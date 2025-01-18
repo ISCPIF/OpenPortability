a50e3aaa0db5684e7cce7253c4da13eeffa610e7
FannyCaulfield <fanny.fougeard@gmail.com>
2025-01-18 12:34:29 +0000

    clean commit precedent

--- worker_reconnect/src/utils.ts
+++
@@ -1,8 +0,0 @@
-export function sleep(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-export function randomSleep(min: number, max: number): Promise<void> {
-  const sleepTime = Math.floor(Math.random() * (max - min + 1)) + min;
-  return sleep(sleepTime);
-}
--- worker_reconnect/start-workers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Worker de reconnexion automatique
-export WORKER_ID=reconnect_worker1
-npx ts-node src/index.ts &
-WORKER_PID=$!
-
-# Attendre que le worker se termine
-wait $WORKER_PID
