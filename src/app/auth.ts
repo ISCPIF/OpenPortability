edb7565a28b608c1d551fbf62e24213121e882eb
FannyCaulfield <fanny.fougeard@gmail.com>
2025-01-18 12:36:43 +0000

    bonne url pour callback mastodon

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -22,7 +22,7 @@ async function createMastodonApp(instance: string){
       // TODO: limiter au strict nécessaire
       // https://docs.joinmastodon.org/api/oauth-scopes/#granular
       "scopes": "read",
-      "website": "https://app.beta.v2.helloquitx.com"
+      "website": "https://app.helloquitx.com"
     };
         try {
       const response = await fetch(url, {
