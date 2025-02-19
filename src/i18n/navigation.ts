ee7231ff166dc3d47a0cac87b43ea3794bb429be
FannyCaulfield <fanny.fougeard@gmail.com>
2025-02-19 08:31:43 +0000

    added PT

--- src/i18n/navigation.ts
+++ src/i18n/navigation.ts
@@ -1,6 +1,6 @@
 import {createSharedPathnamesNavigation} from 'next-intl/navigation';
  
-export const locales = ['en', 'fr', 'es', 'it', 'de', 'sv'] as const;
+export const locales = ['en', 'fr', 'es', 'it', 'de', 'sv', 'pt'] as const;
 export const localePrefix = 'always'; // Default
  
 export const {Link, redirect, usePathname, useRouter} =
